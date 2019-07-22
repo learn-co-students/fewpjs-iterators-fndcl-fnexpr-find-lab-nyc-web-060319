6ca8bf957084428055c605c22a52801089dbe04a
Alexander Riccio <test35965+learn@gmail.com>
2019-07-22 10:24:42 -0400

    solve lab

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  
+  const found = record.find(function(record){return record.result === "W";});
+  // console.log(found);
+  return found ? found.year : undefined;
+}
